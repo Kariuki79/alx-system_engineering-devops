866a2a00d711d97b4a31cf5bf1ddc0ba2b9d20b2
Kariuki79 <kariukimwangi75@gmail.com>
2024-03-05 14:02:29 -0800

    0x0B. SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
-#Using puppet to connect without password
-{
-file /etc/ssh/ssh_config':
+# Using puppet to connect without password
+
+file { /etc/ssh/ssh_config':
 ensure => present,
 }
-file_line { Turn off passwd auth': path -> '/etc/ssh/ssh_config',
+
+file_line { Turn off passwd auth':
+path -> '/etc/ssh/ssh_config',
 line => 'PasswordAuthentication no',
 match -> PasswordAuthentication',
 }
+
 file_line { 'Declare identity file':
 path -> /etc/ssh/ssh config',
 line => 'IdentityFile ~/.ssh/school',
