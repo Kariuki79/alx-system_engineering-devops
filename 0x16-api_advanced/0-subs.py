be35d0fdd9ae7a957924bced1cd0a6ef9015befe
Kariuki79 <kariukimwangi75@gmail.com>
2024-05-09 03:00:53 -0700

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ def number_of_subscribers(subreddit):
     """
     import requests
 
-    sub info = requests.get("https:/www.reddit.com/r/{}/about.json"
-            .format(subreddit)
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+            .format(subreddit),
             headers={"User-agent": "My-User-Agent"},
             allow_redirects=False)
     if sub_info.status_code >= 300:
